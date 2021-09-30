fdd4615b9703fc26884775543945920692614881
Maria Bruner <mariab2898@gmail.com>
2021-09-30 18:42:31 -0400

    fixing bugs

--- db.js
+++ db.js
@@ -3,7 +3,11 @@ require('dotenv').config()
 const Sequelize = require("sequelize")
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 })
 
 module.exports = sequelize
